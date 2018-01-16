cf645976bb05784eda1d78107c8444b07719fad3
Corey Cunningham <coreyjjc@me.com>
2018-01-16 12:11:41 -0500

    a few billion

--- week2/Javascript/bday.js
+++ week2/Javascript/bday.js
@@ -1,3 +1,18 @@
+/*
+While You Wait
+Create a birthday countdown.
+
+var daysUntilMyBirthday = 60;
+For every day, print how many days left.
+
+If it's more than 30 days, print a sad message.
+
+If it's less than 30 days, print a message that sounds excited!
+
+If it's less than 5 days, SCREAM IT!
+
+ONCE IT'S YOUR BIRTHDAY, HAVE A PARTY!
+*/
 var daysUntilMyBirthday = 60;
 
 while (daysUntilMyBirthday > 0) {
--- week2/Javascript/time.js
+++ week2/Javascript/time.js
@@ -1,3 +1,37 @@
+/*
+If You Don't Mind, Can I Have The Time?
+Create a program that will tell you the time.
+
+With the following variables...
+
+var HOUR = 8;
+var MINUTE = 50;
+var PERIOD = "AM";
+...your program should print "It's almost 9 in the morning"
+
+And when changed to...
+
+var HOUR = 7;
+var MINUTE = 15;
+var PERIOD = "PM";
+...your program should print "It's just after 7 in the evening"
+
+Challenge:
+If minutes less than 30, "just after" the hour, more than 30, "almost" the next hour
+AM / PM, "in the morning", "in the evening",
+HINT
+
+You can add as many items into console.log as you need (They will be separated by spaces)
+
+Example:
+
+var person = "Jack";
+var me = "Jill";
+console.log("Hello", person, "I am ", me, ".");
+// Hello Jack I am Jill.
+
+*/
+
 var HOUR = 8;
 var MINUTE = 50;
 var PERIOD = "AM";
