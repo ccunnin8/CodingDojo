83fa1e6233d528f37541e027bb0d86c2f2357820
Corey Cunningham <coreyjjc@me.com>
2018-01-16 11:33:06 -0500

    started working on js

---
+++ week2/Javascript/idodelcare.js
@@ -0,0 +1,6 @@
+var [num1, num2, num3, num4] = [2567,321,1,-2345];
+var [str1,str2,str3] = ["hey there","yo whats good","ecma2015"];
+var [bool1,bool2] = [true,false];
+console.log(num1,num2,num3,num4);
+console.log(str1,str2,str3);
+console.log(bool1,bool2);
