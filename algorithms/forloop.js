116b900b96adf3b4778cefdd3c293a04b1f095f9
Corey Cunningham <coreycunningham@RobertSortsiMac.fios-router.home>
2018-01-15 19:22:00 -0500

    dont worry be happy

--- algorithms/forloop.js
+++ algorithms/forloop.js
@@ -1,4 +1,13 @@
+//-52 to -1066
 for (var i = -52; i <= 1066; i++)
 {
   console.log(i);
+}
+
+//don't worry be happy
+function beCheerful() {
+  console.log("good morning!");
+}
+for (var i = 0; i < 98; i++){
+  beCheerful();
 }
