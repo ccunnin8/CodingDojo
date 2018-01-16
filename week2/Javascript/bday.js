662a8b92a82680bf5b8da74613f6c5824e6596a1
Corey Cunningham <coreyjjc@me.com>
2018-01-16 11:47:30 -0500

    bday

---
+++ week2/Javascript/bday.js
@@ -0,0 +1,17 @@
+var daysUntilMyBirthday = 60;
+
+while (daysUntilMyBirthday > 0) {
+  if (daysUntilMyBirthday > 30 ) {
+    console.log(daysUntilMyBirthday + " days until my bday. Such a long time... ;(");
+  }
+  else if (daysUntilMyBirthday < 30 && daysUntilMyBirthday > 5) {
+    console.log(daysUntilMyBirthday + " days left!");
+  }
+  else if (daysUntilMyBirthday < 5) {
+    console.log(daysUntilMyBirthday + " DAYS UNTIL MY BIRTHDAY!");
+  }
+  daysUntilMyBirthday--;
+}
+console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*");
+console.log("♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪");
+console.log("*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«");
