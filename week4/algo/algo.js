a6b133d5d5fe5921408ec9a5899121e924938e26
Corey Cunningham <coreyjjc@me.com>
2018-01-30 10:17:40 -0500

    algo

---
+++ week4/algo/algo.js
@@ -0,0 +1,12 @@
+// This Length, That Value
+// Given two numbers, return array of length num1 with each value num2. Print "Jinx!" if they are same.
+// 
+// function thisLength(num1,num2){
+// 	var array = [];
+// 	for (var i = 0; i < num1; i++){
+// 		array.push(num2);
+// 	}
+// 	return array;
+// }
+//
+// console.log(thisLength(4,3));
